554d1080a36badb25cbee8b8b4363239eefbff5d
Johirul Amin Rizve <johirulaminrizve@gmail.com>
2024-05-30 16:36:47 +0600

    remove rest template

--- src/main/java/com/valhalla/ehrplugin/configuration/AppConfig.java
+++ src/main/java/com/valhalla/ehrplugin/configuration/AppConfig.java
@@ -3,15 +3,9 @@ package com.valhalla.ehrplugin.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @Bean
     public RestClient restClient(){
