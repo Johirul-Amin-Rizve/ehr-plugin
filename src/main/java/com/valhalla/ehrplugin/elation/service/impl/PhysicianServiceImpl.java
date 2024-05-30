7e885b4443ed3e9d58acc1f37345bed346db0cea
Johirul Amin Rizve <johirulaminrizve@gmail.com>
2024-05-30 15:56:29 +0600

    add rest client to PhysicianServiceImpl

--- src/main/java/com/valhalla/ehrplugin/elation/service/impl/PhysicianServiceImpl.java
+++ src/main/java/com/valhalla/ehrplugin/elation/service/impl/PhysicianServiceImpl.java
@@ -2,25 +2,25 @@ package com.valhalla.ehrplugin.elation.service.impl;
 
 import com.valhalla.ehrplugin.elation.service.PhysicianService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class PhysicianServiceImpl implements PhysicianService {
 
     private static final Logger logger = LoggerFactory.getLogger(PhysicianServiceImpl.class);
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
-    private HttpServletRequest request;
+    private final HttpServletRequest request;
+    private final RestClient restClient;
 
     @Value("${elation.api.baseurl}")
     private String baseUrl;
@@ -30,19 +30,13 @@ public class PhysicianServiceImpl implements PhysicianService {
         String authorizationToken = request.getHeader("Authorization");
         logger.info("Received request to fetch physicians. Authorization token: {}", authorizationToken);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", authorizationToken);
-        headers.set("accept", "application/json");
-
-        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
-
         try {
-            ResponseEntity<Object> response = restTemplate.exchange(
-                    baseUrl + "/physicians/",
-                    HttpMethod.GET,
-                    requestEntity,
-                    Object.class
-            );
+            ResponseEntity<Object> response = restClient.get()
+                    .uri(baseUrl + "/physicians")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .header("Authorization", authorizationToken)
+                    .retrieve()
+                    .toEntity(Object.class);
 
             int statusCodeValue = response.getStatusCodeValue();
             HttpStatus statusCode = (HttpStatus) response.getStatusCode();
